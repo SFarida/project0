650617980b99f9a10cba393555e2ba959cd318a1
SFarida <faridagueleu@gmail.com>
2019-08-18 05:38:35 +0100

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp'), //http://gulpjs.com/
+                sass = require("gulp-sass"),//https://www.npmjs.org/package/gulp-sass
+                log = util.log;
+
+gulp.task('default', function() {
+  // place code for your default task here
+});
